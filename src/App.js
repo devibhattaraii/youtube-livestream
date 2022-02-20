0b9e22ec9ce8508167cd94c538bf0b26fd311a1b
Devi Bhattarai <devakabhattaraii@gmail.com>
2022-02-21 01:54:43 +0545

    Youtube video Livestream demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import YouTube from 'react-youtube';
+import Modal from 'react-modal';
+import './styles.css';
 
-function App() {
+const modalStyles = {
+  content: {
+    top: '0%',
+    left: '0%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+// Render function for Prismic headless CMS pages
+function Content() {
+  const [videoUrl, setVideoUrl] = React.useState(
+    'https://www.youtube.com/watch?v=dC39XHuxBXg'
+  );
+  let videoCode;
+  if (videoUrl) {
+    videoCode = videoUrl.split('v=')[1].split('&')[0];
+  }
+
+  const checkElapsedTime = (e) => {
+    console.log(e.target.playerInfo.playerState);
+    const duration = e.target.getDuration();
+    const currentTime = e.target.getCurrentTime();
+    if (currentTime >= duration) {
+      //  Show or hide the Video to display Live stream map
+    }
+  };
+
+  const opts = {
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <YouTube
+      videoId={videoCode}
+      className="container"
+      containerClassName="embed embed-youtube"
+      onStateChange={(e) => checkElapsedTime(e)}
+      opts={opts}
+    />
   );
 }
 
-export default App;
+export default Content;
